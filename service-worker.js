fc72a0dfdea58fa0deecd358cd17698efff398d6
6630180REM <kevin.swenson@hotmail.com>
2025-10-28 12:32:58 -0500

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'clay-county-softball-cache-v1.2'; 
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/Softball-Logo-Noyear.png?v=2',
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
